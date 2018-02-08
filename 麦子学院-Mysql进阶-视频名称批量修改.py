caae505e8650fdd51065ba09ed1a6e287798a132
Jamin2018 <389098898@qq.com>
2018-02-09 03:35:46 +0800

    批量修改"Mysql进阶"这套视频的名称

---
+++ 麦子学院-Mysql进阶-视频名称批量修改.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+url = 'http://www.maiziedu.com/course/335/'
+dir_path = 'E:\迅雷下载\麦子学院-Mysql进阶'
+
+def mp4_title(url):
+    html = requests.get(url)
+    Soup = BeautifulSoup(html.content,'html.parser')
+    all_li = Soup.find('ul',class_='lesson-lists').findAll('li')
+    titles = []
+    n = 0
+    for i in all_li:
+        n +=1
+        titles.append(i.find('span', class_='fl').text)
+    return titles
+
+def change_files_name(dir_path,url):
+    os.chdir(dir_path)   # 移动到该目录下
+    titles = mp4_title(url)
+    n = 0
+    for title in titles:
+        n += 1
+        os.rename('jinjie'+str(n)+'.mp4',title+'.mp4')
+change_files_name(dir_path,url)
+# mp4_title(url)
